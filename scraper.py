8b422a1683e21a6e131a660e7197ce2ea843f9bf
Mykolazaz <mykolas.motiejunas@gmail.com>
2025-12-26 19:20:25 +0200

    Additional variables to scrape

--- scraper.py
+++ scraper.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 import regex as re
 import pandas as pd
 
+from datetime import datetime
+
 
 TIMEOUT = 10
 MAIN_PAGE = 'https://www.skelbiu.lt/skelbimai/?keywords=iphone&autocompleted=1&search=1&distance=0&mainCity=1&category_id=480&user_type=0&ad_since_max=0&detailsSearch=0&type=1&facets=1&facets=0'
@@ -23,6 +25,21 @@ wait.until(EC.element_to_be_clickable((By.ID, 'showOrderTitle'))).click()
 
 wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='orderByLinks']//span[text()='Naujausi viršuje']"))).click()
 
+lt_months = {
+    "sausį": "01",
+    "vasarį": "02",
+    "kovą": "03",
+    "balandį": "04",
+    "gegužę": "05",
+    "birželį": "06",
+    "liepą": "07",
+    "rugpjūtį": "08",
+    "rugsėjį": "09",
+    "spalį": "10",
+    "lapkritį": "11",
+    "gruodį": "12",
+}
+
 LISTINGS_TO_VISIT = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'li.change-and-submit.active > span'))).text
 
 replacements = str.maketrans({'(':'',
@@ -37,6 +54,7 @@ LISTINGS_TO_VISIT = int(LISTINGS_TO_VISIT)
 PAGES_TO_VISIT = LISTINGS_TO_VISIT // 24
 
 columns = ['id',
+           'url',
             'name',
             'price',
             'maker',
@@ -45,7 +63,10 @@ columns = ['id',
             'city',
             'views',
             'likes',
-            'description']
+            'description',
+            'last_update',
+            'registration_date',
+            'n_listings']
 
 allObjects = pd.DataFrame(columns=columns)
 
@@ -67,10 +88,11 @@ for page in range(1, PAGES_TO_VISIT + 1):
 
     pageListingLinks = list(filter(lambda x : re.search(r'^https://www\.skelbiu\.lt/', x), pageListingLinks))
 
-    for i, url in enumerate(pageListingLinks):
+    for url in pageListingLinks:
         driver.get(url)
 
         objID = None
+        objURL = None
         objName = None
         objPrice = None
         objMaker = None
@@ -80,14 +102,19 @@ for page in range(1, PAGES_TO_VISIT + 1):
         objViews = None
         objLikes = None
         objDescription = None
+        objSellerDate = None
+        objSellerAds = None
+        objLastUpdate = None
 
         wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
 
         if len(driver.find_elements(By.CSS_SELECTOR, 'div.info-title')) == 0:
 
-            objID = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.image-item-id'))).text
-            objID = objID.replace('ID: ', '')
+            objID = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.actions-container > div.block:nth-child(1)'))).text
+            objID = re.search(r'\d{8}', objID).group()
             objID = int(objID)
+
+            objURL = url
 
             objName = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.left-block > h1'))).text
             
@@ -112,9 +139,27 @@ for page in range(1, PAGES_TO_VISIT + 1):
 
             objDescription = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.description.without-web'))).text
 
+            objSellerDate = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.registration-date'))).text
+
+            objSellerDate = objSellerDate.replace("Užsiregistravo ", "")
+
+            for lt, num in lt_months.items():
+                if lt in objSellerDate:
+                    objSellerDate = objSellerDate.replace(lt, num)
+                    break
+
+            objSellerDate = datetime.strptime(objSellerDate, "%Y %m")
+
+            objSellerAds = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.profile-stats'))).text
+            objSellerAds = re.search(r'\d+', objSellerAds).group()
+
+            objLastUpdate = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.actions-container > div.block:nth-child(2)'))).text
+
+
             allObjects.loc[rowCounter] = None
 
             allObjects.loc[rowCounter, 'id'] = objID
+            allObjects.loc[rowCounter, 'url'] = objURL
             allObjects.loc[rowCounter, 'name'] = objName
             allObjects.loc[rowCounter, 'price'] = objPrice
             allObjects.loc[rowCounter, 'maker'] = objMaker
@@ -124,6 +169,11 @@ for page in range(1, PAGES_TO_VISIT + 1):
             allObjects.loc[rowCounter, 'views'] = objViews
             allObjects.loc[rowCounter, 'likes'] = objLikes
             allObjects.loc[rowCounter, 'description'] = objDescription
+            allObjects.loc[rowCounter, 'last_update'] = objLastUpdate
+            allObjects.loc[rowCounter, 'registration_date'] = objSellerDate
+            allObjects.loc[rowCounter, 'n_listings'] = objSellerAds
+            
+
             rowCounter += 1
 
         driver.back()
